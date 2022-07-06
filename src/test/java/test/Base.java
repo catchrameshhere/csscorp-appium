91b42ef3b57ede210e8e18eaef9d638610fdd386
Ramesh A <catchrameshhere@gmai.com>
2022-07-06 09:41:10 +0530

    added the rest of appium codes

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -8,13 +8,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+//import io.appium.java_client.M
 
-public class BasicExample {
+public class Base {
 	
 //	WebDriver driver;
 //	AndroidDriver driver;
 	AndroidDriver driver;
+//	AppiumDriver driver;
 	String hubUrl = "http://127.0.0.1:4723/wd/hub"; 
 	
 	
@@ -31,16 +34,31 @@ public class BasicExample {
 		
 		try {
 			driver = new AndroidDriver(new URL(hubUrl),cap);
+//			driver = new AppiumDriver(new URL(hubUrl),cap);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void setupAndroidEmulator() {
+		DesiredCapabilities cap = new DesiredCapabilities();
+
+		
+		cap.setCapability("platformName", "Android");
+		cap.setCapability("platformVersion", "11");
+		cap.setCapability("deviceName", "ce7b260f");
+		cap.setCapability("appPackage", "com.google.android.calculator");
+		cap.setCapability("appActivity", "com.google.android.calculator.Calculator");
+		
+		try {
+			driver = new AndroidDriver(new URL(hubUrl),cap);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
 		
 	}
 
-	public static void main(String[] args) {
-		BasicExample obj = new BasicExample();
-		obj.setupAndroid();
-
-	}
+	
 
 }
